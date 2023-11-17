67bf246a9613b8a570961544f5ffc50a298984c0
zackaids <zalkar.ai77@gmail.com>
2023-11-17 00:13:59 -0500

    fixed the minesweeper.js function

--- minesweeper.js
+++ minesweeper.js
@@ -1,17 +1,33 @@
+//  let's make markzynk make a bot for a leaderboard kinda thing thoughts ? 
+
+
 // logic
 
+
+const TILE_STATUSES = {
+    HIDDEN: 'hidden',
+    MINE: 'mine',
+    NUMBER: 'number',
+    MARKED: 'marked',
+}
+
+
 export function createBoard(boardSize, numOfMines) {
-    const board = [];
+    const board = []
     for (let x = 0; x < boardSize; x++) {
-        const row = [];
+        const row = []
         for (let y = 0; y < boardSize; y++) {
+            const element = document.createElement("div")
+            element.dataset.status = TILE_STATUSES.HIDDEN
+
             const tile = {
+                element,
                 x,
                 y
             }
-            row.push(tile);
+            row.push(tile)
         }
-        board.push(row);
+        board.push(row)
     }
-    return board;
+    return board
 }
--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 // display
 
-import { createBoard } from "./minesweeper";
+// Importing the createBoard function from minesweeper.js
+import { createBoard } from "./minesweeper.js";
 
-console.log(createBoard(2,2))
-// -populate a board with tiles/mines
+
+// populate a board with tiles/mines
+const board = createBoard(2, 2)
+const boardElement = document.querySelector('.board')
+board.forEach(row => {
+    row.forEach(tile => {
+        boardElement.append(tile.element)
+    })
+})
+
 
 // -method to left click on tiles
-    // -reveal tile
+// -reveal tile
 // -method to right click on tiles
-    // -mark tiles
+// -mark tiles
 // method to check for the end of the game 
