14f068ebba9da789c5c652fc5871c75fc0a301f8
zackaids <zalkar.ai77@gmail.com>
2023-11-17 23:30:10 -0500

    finish the game from tutorial, time to adjust

--- script.js
+++ script.js
@@ -1,21 +1,85 @@
 // display
 
 // Importing the createBoard function from minesweeper.js
-import { createBoard } from "./minesweeper.js";
+import {
+    TILE_STATUSES,
+    createBoard,
+    markTile,
+    revealTile,
+    checkWin,
+    checkLose
+} from "./minesweeper.js";
 
+// -- populate a board with tiles/mines
+// constant variables 
+const BOARD_SIZE = 16
+const NUMBER_OF_MINES = 40
 
-// populate a board with tiles/mines
-const board = createBoard(2, 2)
+// 9x9 10 mines
+// 16x16 40 mines
+// 30x16 99 mines 
+
+// calling the variables 
+const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
 const boardElement = document.querySelector('.board')
+const minesLeftText = document.querySelector('[data-mine-count]')
+const messageText = document.querySelector('.subtext')
+
+// setting up the board
 board.forEach(row => {
     row.forEach(tile => {
         boardElement.append(tile.element)
+        // -- method to left click on tiles
+        tile.element.addEventListener('click', () => {
+            // imported revealTile function for revealing tiles
+            revealTile(board, tile)
+            checkGameEnd()
+        })
+        // -- method to right click on tiles
+        tile.element.addEventListener('contextmenu', e => {
+            e.preventDefault()
+            // imported markTile function for marking tiles
+            markTile(tile)
+            listMinesLeft()
+        })
     })
 })
+boardElement.style.setProperty('--size', BOARD_SIZE)
+minesLeftText.textContent = NUMBER_OF_MINES
+
+function listMinesLeft() {
+    const markedTilesCount = board.reduce((count, row) => {
+        return count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length
+    }, 0)
+
+    minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount
+}
 
 
-// -method to left click on tiles
-// -reveal tile
-// -method to right click on tiles
-// -mark tiles
-// method to check for the end of the game 
+// -- method to check for the end of the game 
+function checkGameEnd() {
+    const win = checkWin(board)
+    const lose = checkLose(board)
+
+    if (win || lose) {
+        boardElement.addEventListener('click', stopProp, { capture: true })
+        boardElement.addEventListener('contextmenu', stopProp, { capture: true })
+    }
+
+    if (win) {
+        messageText.textContent = 'YOU WIN'
+    }
+    if (lose) {
+        messageText.textContent = 'YOU LOSE'
+        board.forEach(row => {
+            row.forEach(tile => {
+                if (tile.TILE_STATUSES === TILE_STATUSES.MARKED) markTile(tile)
+                if (tile.mine) revealTile(board, tile)
+            })
+        })
+    }
+}
+
+function stopProp(e) {
+    e.stopImmediatePropagation()
+}
