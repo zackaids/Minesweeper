cede46827d344a5a60233e54d59b5c474ad9f83c
zackaids <zalkar.ai77@gmail.com>
2023-11-18 23:59:49 -0500

    remaking the minesweeper, logic done

--- script.js
+++ script.js
@@ -1,123 +1,142 @@
-// display
-
-// Importing the createBoard function from minesweeper.js
-import {
-    TILE_STATUSES,
-    createBoard,
-    markTile,
-    revealTile,
-    checkWin,
-    checkLose
-} from "./minesweeper.js";
-
-// -- populate a board with tiles/mines
-// constant variables 
-let boardWidth
-let boardHeight
-let numberOfMines
-
 // 9x9 10 mines
 // 16x16 40 mines
 // 30x16 99 mines 
 
-// calling the variables 
-let board
+const gameSettings = {
+    'beginner': {
+        'gridWidth': 9,
+        'gridHeight': 9,
+        'maxMines': 10,
+    },
+    'intermediate': {
+        'gridWidth': 16,
+        'gridHeight': 16,
+        'maxMines': 40,
+    },
+    'advanced': {
+        'gridWidth': 30,
+        'gridHeight': 16,
+        'maxMines': 99,
+    }
+}
+
 const boardElement = document.querySelector('.board')
 const minesLeftText = document.querySelector('[data-mine-count]')
-const messageText = document.querySelector('.subtext')
+
+let numberOfMines
 
 
-// setting up the board
-function renderBoard() {
+
+// function to create board
+function createBoard(gameSettings) {
+    let board = [];
+    for (let y = 0; y < gameSettings.gridHeight; y++) {
+        const row = [];
+        for (let x = 0; x < gameSettings.gridWidth; x++) {
+            let cell = 0;
+            row.push(cell);
+        }
+        board.push(row);
+    }
+
+    for (let i = 0; i < gameSettings.maxMines; i++) {
+        placeMine(board);
+    }
+
+    return board;
+}
+
+
+
+
+// function to place a mine at a random place in a 2d array
+function placeMine(board) {
+    const height = board.length;
+    const width = board[0].length;
+    const randomY = Math.floor(Math.random() * height);
+    const randomX = Math.floor(Math.random() * width);
+
+    if (board[randomY][randomX] !== 1) {
+        board[randomY][randomX] = 1;
+    } else {
+        placeMine(board);
+    }
+}
+
+
+/*
+
+[
+    [0,0,0 ...],
+    [0,0,0 ...],
+    [0,0,0 ...],
+    [0,0,0 ...],
+    [0,0,0 ...],
+    [0,0,0 ...]
+]
+
+*/
+
+// rendering board
+function renderBoard(board) {
     board.forEach(row => {
-        row.forEach(tile => {
-            boardElement.append(tile.element)
+        row.forEach(cell => {
+            const element = document.createElement("div");
+            boardElement.append(element)
             // -- method to left click on tiles
-            tile.element.addEventListener('click', () => {
-                // imported revealTile function for revealing tiles
-                revealTile(board, tile)
-                checkGameEnd()
-            })
-            // -- method to right click on tiles
-            tile.element.addEventListener('contextmenu', e => {
-                e.preventDefault()
-                // imported markTile function for marking tiles
-                markTile(tile)
-                listMinesLeft()
-            })
+            // tile.element.addEventListener('click', () => {
+            //     // imported revealTile function for revealing tiles
+            //     revealTile(board, tile)
+            //     checkGameEnd()
+            // })
+            // // -- method to right click on tiles
+            // tile.element.addEventListener('contextmenu', e => {
+            //     e.preventDefault()
+            //     // imported markTile function for marking tiles
+            //     markTile(tile)
+            //     listMinesLeft()
+            // })
         })
     })
-    boardElement.style.setProperty('--size-width', boardWidth)
-    boardElement.style.setProperty('--size-height', boardHeight)
+    boardElement.style.setProperty('--size-width', gameSettings.gridWidth)
+    boardElement.style.setProperty('--size-height', gameSettings.gridHeight)
     minesLeftText.textContent = numberOfMines
     boardElement.style.visibility = "visible"
 }
 
-function clearBoard() {
-    boardElement.innerHTML = ""
-    boardElement.style.visibility = "hidden"
-}
-function listMinesLeft() {
-    const markedTilesCount = board.reduce((count, row) => {
-        return count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length
-    }, 0)
+let newboard = createBoard(gameSettings.beginner);
+renderBoard(newboard);
 
-    minesLeftText.textContent = numberOfMines - markedTilesCount
-}
+// function placeMine2(board) {
+//     const height = board.length;
+//     const width = board[0].length;
 
+//     let randomY;
+//     let randomX;
 
-// -- method to check for the end of the game 
-function checkGameEnd() {
-    const win = checkWin(board)
-    const lose = checkLose(board)
+//     let condition = true
+//     while(condition) {
+//         randomY = Math.floor(Math.random() * height);
+//         randomX = Math.floor(Math.random() * width);
 
-    if (win || lose) {
-        boardElement.addEventListener('click', stopProp, { capture: true })
-        boardElement.addEventListener('contextmenu', stopProp, { capture: true })
-    }
+//         if (board[randomY][randomX] != 1) {
+//             board[randomY][randomX] = 1;
+//             condition = false
+//         }
+//     }
+// }
 
-    if (win) {
-        messageText.textContent = 'YOU WIN'
-    }
-    if (lose) {
-        messageText.textContent = 'YOU LOSE'
-        board.forEach(row => {
-            row.forEach(tile => {
-                if (tile.TILE_STATUSES === TILE_STATUSES.MARKED) markTile(tile)
-                if (tile.mine) revealTile(board, tile)
-            })
-        })
-    }
-}
+/*
+setneterce. trying
 
-function stopProp(e) {
-    e.stopImmediatePropagation()
-}
+*/
+
+// function to erase board
+
+// function to right click on tile
+
+// function to left click on tile
+
+// function to check game win/game lose
 
 
-document.getElementById("beginner").addEventListener("click", () => {
-    boardWidth = 9
-    boardHeight = 9
-    numberOfMines = 10
-    board = createBoard(boardWidth, boardHeight, numberOfMines)
-    clearBoard()
-    renderBoard()
-})
-
-document.getElementById("intermediate").addEventListener("click", () => {
-    boardWidth = 16
-    boardHeight = 16
-    numberOfMines = 40
-    board = createBoard(boardWidth, boardHeight, numberOfMines)
-    clearBoard()
-    renderBoard()
-})
-
-document.getElementById("expert").addEventListener("click", () => {
-    boardWidth = 30
-    boardHeight = 16
-    numberOfMines = 99
-    board = createBoard(boardWidth, boardHeight, numberOfMines)
-    clearBoard()
-    renderBoard()
-})
