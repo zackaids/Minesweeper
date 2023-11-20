23bb215eeac503d95f25379f0c1c1c9b35abaa2d
zackaids <zalkar.ai77@gmail.com>
2023-11-19 22:49:47 -0500

    calculateAdjacentMines() function

--- script.js
+++ script.js
@@ -27,13 +27,17 @@ let numberOfMines
 
 
 
-// function to create board
 function createBoard(gameSettings) {
     let board = [];
     for (let y = 0; y < gameSettings.gridHeight; y++) {
         const row = [];
         for (let x = 0; x < gameSettings.gridWidth; x++) {
-            let cell = 0;
+            let cell = {
+                isMine: false,
+                isRevealed: false,
+                isFlagged: false,
+                adjacentMines: 0,
+            }
             row.push(cell);
         }
         board.push(row);
@@ -43,42 +47,28 @@ function createBoard(gameSettings) {
         placeMine(board);
     }
 
-    return board;
+    calculateAdjacentMines(board);
+
+    return { board, gameSettings };
 }
 
 
-
-
-// function to place a mine at a random place in a 2d array
 function placeMine(board) {
     const height = board.length;
     const width = board[0].length;
     const randomY = Math.floor(Math.random() * height);
     const randomX = Math.floor(Math.random() * width);
 
-    if (board[randomY][randomX] !== 1) {
-        board[randomY][randomX] = 1;
+    if (!board[randomY][randomX].isMine) {
+        board[randomY][randomX].isMine = true;
     } else {
         placeMine(board);
     }
 }
 
 
-/*
 
-[
-    [0,0,0 ...],
-    [0,0,0 ...],
-    [0,0,0 ...],
-    [0,0,0 ...],
-    [0,0,0 ...],
-    [0,0,0 ...]
-]
-
-*/
-
-// rendering board
-function renderBoard(board) {
+function renderBoard(board, gameSettings) {
     board.forEach(row => {
         row.forEach(cell => {
             const element = document.createElement("div");
@@ -100,12 +90,45 @@ function renderBoard(board) {
     })
     boardElement.style.setProperty('--size-width', gameSettings.gridWidth)
     boardElement.style.setProperty('--size-height', gameSettings.gridHeight)
+
     minesLeftText.textContent = numberOfMines
     boardElement.style.visibility = "visible"
 }
 
-let newboard = createBoard(gameSettings.beginner);
-renderBoard(newboard);
+function calculateAdjacentMines(board) {
+    const height = board.length;
+    const width = board[0].length;
+
+    for (let y = 0; y < height; y++) {
+        for (let x = 0; x < width; x++) {
+            let adjacentMines = 0;
+
+            // the previous for loop iterates through each cell
+            // these next for loops iterate through each cell within the one square radius
+            for (let dy = -1; dy <= 1; dy++) {
+                for (let dx = -1; dx <= 1; dx++) {
+                    const newY = y + dy;
+                    const newX = x + dx;
+
+                    // this if statement is needed to make sure that the new x and y
+                    // are not out of bounds 
+                    // (if new x/y=0 or greater than/equal to width/height)
+                    if (newY >= 0 && newY < height && newX >= 0 && newX < width) {
+                        if (board[newY][newX].isMine) {
+                            adjacentMines++;
+                        }
+                    }
+                }
+            }
+
+            board[y][x].adjacentMines = adjacentMines;
+        }
+    }
+}
+
+
+let { board: newBoard, gameSettings: currentGameSettings } = createBoard(gameSettings.beginner);
+renderBoard(newBoard, currentGameSettings);
 
 // function placeMine2(board) {
 //     const height = board.length;
@@ -126,17 +149,19 @@ renderBoard(newboard);
 //     }
 // }
 
-/*
-setneterce. trying
 
-*/
 
 // function to erase board
 
-// function to right click on tile
+// isFlagged() function to right click on tile
 
 // function to left click on tile
+// isRevealed() reveal cell (when cell is revealed, check for mine)
 
 // function to check game win/game lose
+
+// function to reset game
+
+// better UI
 
 
